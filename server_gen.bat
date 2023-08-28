9fa1e8fe511667da8b65324b160da9e262070ac0
Jessie Snedden <JessieSnedden@pm.me>
2023-08-28 03:05:04 +0100

    Fixed batch file append to cfg, corrected README

--- server_gen.bat
+++ server_gen.bat
@@ -1,10 +1,10 @@
 @echo off
 
 IF NOT EXIST steam_appid.txt (
-    @echo 228380 >steam_appid.txt
+    @echo 228380 > steam_appid.txt
 )
 
-server_config.cfg > active_config.cfg
-.\eventloops\events.py >> active_config.cfg
+copy server_config.cfg active_config.cfg
+py .\eventloops\events.py racing | Add-Content -Path active_config.cfg -Encoding Oem
 
 start /B Wreckfest_x64.exe -s server_config=active_config.cfg
